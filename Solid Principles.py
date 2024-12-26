1d7766b98b0c4f9b76802d5b8eaae85a82fca019
mayarehab94 <mayar.ehab823@ai.mnu.edu.eg>
2024-12-26 05:11:06 +0200

    Update Solid Principles.py

--- Solid Principles.py
+++ Solid Principles.py
@@ -4,22 +4,29 @@
 # In[ ]:
 
 
-class CreditCardPayment:
-    def process_payment(self, amount):
+# this code violate the SOLID principles
+class PaymentMethod:
+    def CreditCardPayment(self, amount):
         print(f"Processing credit card payment of {amount}.")
-
-class PayPalPayment:
-    def process_payment(self, amount):
+        
+    def PayPalPayment(self, amount):
         print(f"Processing PayPal payment of {amount}.")
+        
 
 class PaymentProcessor:
     def __init__(self, payment_method):
         self.payment_method = payment_method
+        
+    def process_CreditCard(self, amount):
+        self.payment_method.CreditCardPayment(amount)
 
-    def process(self, amount):
-        self.payment_method.process_payment(amount)
+    def process_PayPal(self, amount):
+        self.payment_method.PayPalPayment(amount)
 
+#This code dosenot violate the SOLID princible
+from abc import ABC , abstractmethod 
 class PaymentMethod:
+    @abstractmethod
     def process_payment(self, amount):
         pass
 
@@ -30,6 +37,7 @@ class CreditCardPayment(PaymentMethod):
 class PayPalPayment(PaymentMethod):
     def process_payment(self, amount):
         print(f"Processing PayPal payment of {amount}.")
+        
 
 class PaymentProcessor:
     def __init__(self, payment_method: PaymentMethod):
@@ -45,4 +53,5 @@ payment_processor.process(150)
 payment_method = PayPalPayment()
 payment_processor = PaymentProcessor(payment_method)
 payment_processor.process(250)
+
 
